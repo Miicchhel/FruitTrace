1bd63a39fc8941e2f941d89a658cc9c6ea6922a3
michel ferreira da silva melo <michel.ferreira.melo@gmail.com>
2024-05-22 12:53:04 -0300

    adicionando os campos do formulário e o btn de cadastrar usuário

--- lib/src/auth/sign_up_screen.dart
+++ lib/src/auth/sign_up_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:fruittrace/src/auth/components/custom_text_field.dart';
 import 'package:fruittrace/src/auth/config/custom_colors.dart';
 
 class SignUpScreen extends StatelessWidget {
@@ -8,24 +10,85 @@ class SignUpScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: CustomColors.customSwatchColor,
-      body: const Column(
+      body: Column(
         children: [
-
           // Titulo
-          Expanded(
+          const Expanded(
             child: Center(
               child: Text(
                 'Cadastro',
                 style: TextStyle(
                   fontSize: 35.0,
-                  color: Colors.white
+                  color: Colors.white,
+                  shadows: [
+                    Shadow(
+                        color: Color.fromARGB(255, 68, 67, 67),
+                        offset: Offset(2.0, 2.0),
+                        blurRadius: 6.0),
+                  ],
                 ),
               ),
             ),
           ),
 
           // Formulario
+          Container(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 32.0, vertical: 40.0),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.vertical(top: Radius.circular(50.0)),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const CustomTextField(
+                  icon: Icons.person,
+                  label: 'Nome',
+                ),
+                const CustomTextField(
+                  icon: Icons.email,
+                  label: 'Email',
+                ),
+                const CustomTextField(
+                  icon: Icons.lock,
+                  label: 'Senha',
+                  isSecret: true,
+                ),
+                const CustomTextField(
+                  icon: Icons.phone,
+                  label: 'Celular',
+                ),
+                const CustomTextField(
+                  icon: Icons.file_copy,
+                  label: 'CPF',
+                ),
+                SizedBox(
+                  height: 50.0,
+                  child: ElevatedButton(
 
+                    onPressed: () {},
+
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(18.0)),
+                      ),
+                    ),
+
+                    child: const Text(
+                      'Cadastrar usuário',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.white
+                      ),
+                    ),
+
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
