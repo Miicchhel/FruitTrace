8b6e11cfcadc4b0fff3aa004550c156b3cf7da2d
michel ferreira da silva melo <michel.ferreira.melo@gmail.com>
2024-05-22 12:55:52 -0300

    resolvendo o problema de overflow

--- lib/src/auth/sign_up_screen.dart
+++ lib/src/auth/sign_up_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:fruittrace/src/auth/components/custom_text_field.dart';
 import 'package:fruittrace/src/auth/config/custom_colors.dart';
 
@@ -8,88 +7,97 @@ class SignUpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: CustomColors.customSwatchColor,
-      body: Column(
-        children: [
-          // Titulo
-          const Expanded(
-            child: Center(
-              child: Text(
-                'Cadastro',
-                style: TextStyle(
-                  fontSize: 35.0,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Column(
+            children: [
+              // Titulo
+              const Expanded(
+                child: Center(
+                  child: Text(
+                    'Cadastro',
+                    style: TextStyle(
+                      fontSize: 35.0,
+                      color: Colors.white,
+                      shadows: [
+                        Shadow(
+                            color: Color.fromARGB(255, 68, 67, 67),
+                            offset: Offset(2.0, 2.0),
+                            blurRadius: 6.0),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+          
+              // Formulario
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 32.0, vertical: 40.0),
+                decoration: const BoxDecoration(
                   color: Colors.white,
-                  shadows: [
-                    Shadow(
-                        color: Color.fromARGB(255, 68, 67, 67),
-                        offset: Offset(2.0, 2.0),
-                        blurRadius: 6.0),
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(50.0)),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const CustomTextField(
+                      icon: Icons.person,
+                      label: 'Nome',
+                    ),
+                    const CustomTextField(
+                      icon: Icons.email,
+                      label: 'Email',
+                    ),
+                    const CustomTextField(
+                      icon: Icons.lock,
+                      label: 'Senha',
+                      isSecret: true,
+                    ),
+                    const CustomTextField(
+                      icon: Icons.phone,
+                      label: 'Celular',
+                    ),
+                    const CustomTextField(
+                      icon: Icons.file_copy,
+                      label: 'CPF',
+                    ),
+                    SizedBox(
+                      height: 50.0,
+                      child: ElevatedButton(
+          
+                        onPressed: () {},
+          
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(18.0)),
+                          ),
+                        ),
+          
+                        child: const Text(
+                          'Cadastrar usuário',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.white
+                          ),
+                        ),
+          
+                      ),
+                    ),
                   ],
                 ),
               ),
-            ),
+            ],
           ),
-
-          // Formulario
-          Container(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 32.0, vertical: 40.0),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(top: Radius.circular(50.0)),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const CustomTextField(
-                  icon: Icons.person,
-                  label: 'Nome',
-                ),
-                const CustomTextField(
-                  icon: Icons.email,
-                  label: 'Email',
-                ),
-                const CustomTextField(
-                  icon: Icons.lock,
-                  label: 'Senha',
-                  isSecret: true,
-                ),
-                const CustomTextField(
-                  icon: Icons.phone,
-                  label: 'Celular',
-                ),
-                const CustomTextField(
-                  icon: Icons.file_copy,
-                  label: 'CPF',
-                ),
-                SizedBox(
-                  height: 50.0,
-                  child: ElevatedButton(
-
-                    onPressed: () {},
-
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(18.0)),
-                      ),
-                    ),
-
-                    child: const Text(
-                      'Cadastrar usuário',
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        color: Colors.white
-                      ),
-                    ),
-
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
