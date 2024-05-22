791455eba1b12e3b238d4a66eb068033b2a1f830
michel ferreira da silva melo <michel.ferreira.melo@gmail.com>
2024-05-22 12:22:21 -0300

    navegando para a tela de cadastro de usuário

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -2,6 +2,7 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:fruittrace/src/auth/components/custom_text_field.dart';
 import 'package:fruittrace/src/auth/config/custom_colors.dart';
+import 'package:fruittrace/src/auth/sign_up_screen.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -24,7 +25,7 @@ class _SignInScreenState extends State<SignInScreen> {
           width: size.width,
           child: Column(
             children: [
-              // parte de cimay
+              // parte de cima
               Expanded(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -206,7 +207,12 @@ class _SignInScreenState extends State<SignInScreen> {
                     SizedBox(
                       height: 50.0,
                       child: OutlinedButton(
-                        onPressed: (){},
+
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(builder: (context) => const SignUpScreen())
+                          );
+                        },
                       
                         style: OutlinedButton.styleFrom(
                           shape: const RoundedRectangleBorder(
