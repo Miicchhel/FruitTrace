c8ead279634f9eb607ddf417c4aa46dad97d40e5
michel ferreira da silva melo <michel.ferreira.melo@gmail.com>
2024-05-21 23:41:31 -0300

    criando o botão de entrar

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import 'package:fruittrace/src/auth/components/custom_text_field.dart';
 
 class SignInScreen extends StatefulWidget {
@@ -31,19 +33,44 @@ class _SignInScreenState extends State<SignInScreen> {
                   top: Radius.circular(50.0),
                 ),
               ),
-              child: const Column(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   // Email
-                  CustomTextField(
+                  const CustomTextField(
                     icon: Icons.email,
                     label: 'Email'
                   ),
                   
-                  //Senha
-                  CustomTextField(
+                  // Senha
+                  const CustomTextField(
                     icon: Icons.lock,
                     label: 'Senha',
                     isSecret: true,
+                  ),
+
+                  // btn Entrar
+                  SizedBox(
+                    height: 50.0,
+                    child: ElevatedButton(
+                      onPressed: () {},
+
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(18.0)),
+                        )
+                      ),
+
+                      child: const Text(
+                        'Entrar', 
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
