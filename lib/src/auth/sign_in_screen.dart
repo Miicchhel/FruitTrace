33395e016a45dc200c5026d8014d45d344f617e2
michel ferreira da silva melo <michel.ferreira.melo@gmail.com>
2024-05-22 22:33:41 -0300

    realocando o diretório "config" fora da pasta "auth"

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,7 +1,7 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:fruittrace/src/auth/components/custom_text_field.dart';
-import 'package:fruittrace/src/auth/config/custom_colors.dart';
+import 'package:fruittrace/src/config/custom_colors.dart';
 import 'package:fruittrace/src/auth/sign_up_screen.dart';
 import 'package:fruittrace/src/base/base_screen.dart';
 
