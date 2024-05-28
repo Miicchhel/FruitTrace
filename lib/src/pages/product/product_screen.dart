f96a09bfe6142b06a8f2e9ba5f462d45b077a4fe
michel ferreira da silva melo <michel.ferreira.melo@gmail.com>
2024-05-28 18:34:51 -0300

    apresentação da imagem e montando o container

---
+++ lib/src/pages/product/product_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:fruittrace/src/models/item_model.dart';
+
+class ProductScreen extends StatelessWidget {
+  final ItemModel item;
+
+  const ProductScreen({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white.withAlpha(230),
+
+      body: Column(
+        children: [
+          Expanded(child: Image.asset(item.imgUrl)),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(50.0),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade600,
+                    offset: const Offset(0.0, 1.0),
+                    blurRadius: 6.0,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
