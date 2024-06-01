22dd7c83fdab8f51716660ef2453173e8ce23e17
michel ferreira da silva melo <michel.ferreira.melo@gmail.com>
2024-06-01 19:13:19 -0300

    adicionando o total e o btn de confirmação de pedidos

---
+++ lib/src/pages/cart/cart_tab.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:fruittrace/src/config/custom_colors.dart';
+import 'package:fruittrace/src/services/utils_services.dart';
+
+class CartTab extends StatefulWidget {
+  CartTab({super.key});
+
+  @override
+  State<CartTab> createState() => _CartTabState();
+}
+
+class _CartTabState extends State<CartTab> {
+
+  final UtilsServices utilsServices = UtilsServices();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: CustomColors.customSwatchColor[200],
+        centerTitle: true,
+        title: const Text(
+          'Carrinho',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            shadows: [
+              Shadow(
+                color: Color.fromARGB(255, 68, 67, 67),
+                offset: Offset(2.0, 2.0),
+                blurRadius: 4.0
+              ),
+            ],
+          ),
+        ),
+      ),
+
+      body: Column(
+        children: [
+          // lista de itens do carrinho
+          const Expanded(child: Placeholder()),
+
+          // const SizedBox(height: 20),
+
+          // Total e confirmação de pedido
+          Container(
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(30.0),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade300,
+                  spreadRadius: 3.0,
+                  blurRadius: 2,
+                ),
+              ],
+            ),
+
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Text(
+                  'Total Geral (teste)',
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+
+                Text(
+                  utilsServices.priceToCurrency(50.5),
+                  style: TextStyle(
+                    fontSize: 23.0,
+                    color: CustomColors.customSwatchColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                SizedBox(
+                  height: 50.0,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: CustomColors.customSwatchColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                      ),
+                    ),
+                    
+                    onPressed: () {},
+
+                    child: const Text(
+                      'Concluir pedido',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
