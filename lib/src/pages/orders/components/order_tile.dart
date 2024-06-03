e70b4c9c3846934069505ebae969b642101fd9b8
michel ferreira da silva melo <michel.ferreira.melo@gmail.com>
2024-06-03 14:27:32 -0300

    desenvolvendo o título do tile com a data formatada

--- lib/src/pages/orders/components/order_tile.dart
+++ lib/src/pages/orders/components/order_tile.dart
@@ -1,17 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:fruittrace/src/models/order_model.dart';
+import 'package:fruittrace/src/services/utils_services.dart';
 
 class OrderTile extends StatelessWidget {
 
   final OrderModel order;
 
-  const OrderTile({
+  OrderTile({
     super.key,
     required this.order,
   });
 
+  final UtilsServices utilsServices = UtilsServices();
+
   @override
   Widget build(BuildContext context) {
-    return Container(height: 150, color: Colors.red,);
+    return Card(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+        20.0,
+      )),
+      child: Theme(
+        // para excluir o divisor da expansão
+        data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+
+        child: ExpansionTile(
+          title: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Pedido: ${order.id}, '),
+              Text(
+                utilsServices.formatDataTime(order.createdDateTime),
+                style: const TextStyle(fontSize: 12, color: Colors.black54),
+              ),
+            ],
+          ),
+          children: [SizedBox(height: 100,)],
+        ),
+      ),
+    );
   }
 }
